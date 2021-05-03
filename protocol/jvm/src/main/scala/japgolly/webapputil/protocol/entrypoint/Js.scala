95106a309e9a5d1e61d435ca98d85d12bcbdfbfa
David Barri <japgolly@gmail.com>
2021-05-04 09:02:43 +1000

    Add Js.Wrapper composition

--- protocol/jvm/src/main/scala/japgolly/webapputil/protocol/entrypoint/Js.scala
+++ protocol/jvm/src/main/scala/japgolly/webapputil/protocol/entrypoint/Js.scala
@@ -5,9 +5,20 @@ final case class Js(val asString: String) extends AnyVal {
 }
 
 object Js {
+
   final case class Wrapper(before: String, after: String) {
     val totalLength = before.length + after.length
+
+    def around(inside: Wrapper): Wrapper =
+      Wrapper(
+        before = this.before + inside.before,
+        after  = inside.after + this.after,
+      )
+
+    def inside(outer: Wrapper): Wrapper =
+      outer.around(this)
   }
+
   object Wrapper {
     val windowOnLoad = Wrapper("window.onload=function(){", "};") // why'd I add a semi-colon here again? Can't remember...
   }
