3a873a5d366cb62da7a071b6d51cab8fe77c4cc1
David Barri <japgolly@gmail.com>
2022-05-13 15:09:01 +1000

    Add update-gh-pages.sh

---
+++ update-gh-pages.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+cd "$(dirname "$0")"
+
+output="*.html helium examples/*.html"
+
+# Remove old output
+rm -rf $output
+
+sbt clean test ghpages/laikaSite
+
+echo
+cp -rv ghpages/target/docs/site/. .
+echo
+
+git add $output
+git status
+echo "git commit -m 'Refresh ghpages' && git push && git checkout master"
+echo
