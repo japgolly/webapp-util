fd1d621b2620ef2550425b1c7ceeb03139e118ab
David Barri <japgolly@gmail.com>
2022-08-29 19:21:52 +1000

    Make `TestDbConfig` combinable with other test config downstream

---
+++ testDbPostgres/src/main/scala/japgolly/webapputil/db/test/TestDbConfig.scala
@@ -0,0 +1,45 @@
+package japgolly.webapputil.db.test
+
+import cats.Eval
+import japgolly.clearconfig._
+import japgolly.webapputil.db.DbConfig
+
+/** Extend this to create your own:
+ *
+ * {{{
+ *   object TestConfig extends japgolly.webapputil.db.test.TestDbConfig {
+ *     override protected def propsFilename =
+ *       "blah.properties"
+ *   }
+ * }}}
+ */
+trait TestDbConfig {
+
+  protected def propsPrefixDb =
+    "db."
+
+  protected def propsFilename =
+    "test.properties"
+
+  protected def propsFilenameOptional =
+    true
+
+  protected def configSources: ConfigSources[Eval] =
+    // Highest pri
+    ConfigSource.environment[Eval] >
+    ConfigSource.propFileOnClasspath[Eval](propsFilename, optional = propsFilenameOptional) >
+    ConfigSource.system[Eval]
+    // Lowest pri
+
+  protected def loadDbConfig[A](defn: ConfigDef[A]): A =
+    defn.run(configSources).map(_.getOrDie()).value
+
+  protected def dbConfig: ConfigDef[DbConfig] =
+    DbConfig.config.withPrefix(propsPrefixDb)
+
+  protected def loadDbConfigOrThrow(): DbConfig
+    loadDbConfig(dbConfig)
+
+  lazy val db: DbConfig =
+    loadDbConfigOrThrow()
+}
