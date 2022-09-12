e2496e3901c7f64c3622dfe32e5770d488a7b1ab
David Barri <japgolly@gmail.com>
2022-09-12 14:34:21 +1000

    More JsonUtil

--- coreCirce/shared/src/main/scala/japgolly/webapputil/circe/JsonUtil.scala
+++ coreCirce/shared/src/main/scala/japgolly/webapputil/circe/JsonUtil.scala
@@ -1,21 +1,114 @@
 package japgolly.webapputil.circe
 
 import io.circe._
+import io.circe.syntax._
 import japgolly.microlibs.utils.StaticLookupFn
 import japgolly.webapputil.general.ErrorMsg
+import japgolly.univeq._
 
 object JsonUtil {
+
+  object Implicits extends Implicits
+
+  trait Implicits
+    extends CirceExtensions
+       with UnivEqInstances
+
+  // ===================================================================================================================
 
   object UnivEqInstances extends UnivEqInstances
 
   trait UnivEqInstances {
-    import japgolly.univeq.UnivEq
-
     implicit def univEqCirceDecodingFailure: UnivEq[DecodingFailure] = UnivEq.force
     implicit def univEqCirceError          : UnivEq[io.circe.Error]  = UnivEq.force
     implicit def univEqCirceJson           : UnivEq[Json]            = UnivEq.force
     implicit def univEqCirceJsonObject     : UnivEq[JsonObject]      = UnivEq.force
   }
+
+  // ===================================================================================================================
+
+  trait CirceExtensions {
+    import CirceExtensions._
+
+    implicit def webappUtilCirceExtEncoderObject(a: Encoder.type): WebappUtilCirceExtEncoderObject =
+      new WebappUtilCirceExtEncoderObject(a)
+
+    implicit def WebappUtilCirceExtDecoderF[F[_], A](a: Decoder[F[A]]): WebappUtilCirceExtDecoderF[F, A] =
+      new WebappUtilCirceExtDecoderF(a)
+
+    implicit def webappUtilCirceExtHCursor(a: HCursor): WebappUtilCirceExtHCursor =
+      new WebappUtilCirceExtHCursor(a)
+
+    implicit def webappUtilCirceExtJsonObject(a: JsonObject): WebappUtilCirceExtJsonObject =
+      new WebappUtilCirceExtJsonObject(a)
+  }
+
+  object CirceExtensions extends CirceExtensions {
+
+    class WebappUtilCirceExtEncoderObject(private val self: Encoder.type) extends AnyVal {
+
+      def instanceObject[A](f: A => JsonObject): Encoder[A] =
+        Encoder.instance[A](a => Json.fromJsonObject(f(a)))
+    }
+
+    class WebappUtilCirceExtDecoderF[F[_], A](private val self: Decoder[F[A]]) extends AnyVal {
+
+      def orLiftOne(f: A => F[A])(implicit A: Decoder[A]): Decoder[F[A]] =
+        Decoder.instance[F[A]] { c =>
+          val rf = self(c)
+          if (rf.isRight)
+            rf
+          else {
+            val ra = A(c)
+            if (ra.isRight) ra.map(f) else rf
+          }
+        }
+    }
+
+    class WebappUtilCirceExtHCursor(private val self: HCursor) extends AnyVal {
+
+      def decodeSomeOrOne[F[_], A](one: A => F[A])
+                                  (implicit F: Decoder[F[A]], A: Decoder[A]): Decoder.Result[F[A]] =
+        F.orLiftOne(one)(A)(self)
+
+      def getOption[A: Decoder](key: String, nullIsNone: Boolean = true): Decoder.Result[Option[A]] = {
+        val f = self.downField(key)
+        f.focus match {
+          case None                              => Right(None)
+          case Some(j) if nullIsNone && j.isNull => Right(None)
+          case Some(_)                           => f.as[A].map(Some.apply)
+        }
+      }
+
+      def getOrDefault[A: Decoder](key: String, default: A, nullIsNone: Boolean = true): Decoder.Result[A] =
+        getOption(key, nullIsNone = nullIsNone).map(_.getOrElse(default))
+
+      def getSoleField[A: Decoder](key: String): Decoder.Result[A] =
+        self.keys match {
+          case Some(keys) =>
+            keys.iterator.take(2).toList match {
+              case k :: Nil if k ==* key => self.downField(key).as[A]
+              case _                     => Left(DecodingFailure(s"Exactly one key ($key) required in ${self.focus}", self.history))
+            }
+          case None => Left(DecodingFailure("Object expected.", self.history))
+        }
+
+      def getSomeOrOne[F[_], A](key: String, one: A => F[A])
+                               (implicit F: Decoder[F[A]], A: Decoder[A]): Decoder.Result[F[A]] =
+        self.get(key)(F.orLiftOne(one))
+    }
+
+    class WebappUtilCirceExtJsonObject(private val self: JsonObject) extends AnyVal {
+
+      def addOption[A: Encoder](key: String, value: Option[A]): JsonObject =
+        value.fold(self)(a => self.add(key, a.asJson))
+
+      def addWhen[A: Encoder](key: String, value: A)(test: A => Boolean): JsonObject =
+        if (test(value)) self.add(key, value.asJson) else self
+    }
+  }
+
+  // ===================================================================================================================
 
   def decoderFnSumBySoleKey[A](f: PartialFunction[(String, ACursor), Decoder.Result[A]]): ACursor => Decoder.Result[A] = {
     def keyErr = "Expected a single key indicating the subtype"
